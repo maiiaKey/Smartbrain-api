f4eb3029419628c0675f3a8726f8411a664b66fc
maiiaKey <59762050+maiiaKey@users.noreply.github.com>
2020-04-19 19:43:36 +0200

    first local version smartbrain api

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'app64data',
+      database : 'smartbrain'
+    }
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleProfileGet(db))
+app.put('/image', image.handleImage(db))
+app.post('/imageurl', image.handleApiCall)
+
+app.listen(4000, () => {
+    console.log('app is running on port 4000')
+});
